003bd7163c115c4da19565b6c93359658bd2feb2
davidgolay <74565736+davidgolay@users.noreply.github.com>
2022-03-14 21:36:09 +0100

    initial commit

---
+++ LivraisonCoteDorGolay/Logic/networks/City.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivraisonCoteDor.network
+{
+    public class City
+    {
+        #region Attributes
+        private int _id;
+        private string _name;
+        private float _lt;
+        private float _lg;
+        #endregion
+
+        #region Properties
+        public int Id { get => this._id; }
+
+        public string Name { get => this._name; }
+
+        public float Longitude { get => this._lg; }
+
+        public float Latitude { get => this._lt; }
+        #endregion
+
+        public City(int id, String name, float lt, float lg)
+        {
+            this._id = id;
+            this._name = name;
+            this._lt = lt;
+            this._lg = lg;
+        }
+
+    }
+
+}
---
+++ LivraisonCoteDorGolay/Logic/readers/Reader.cs
@@ -0,0 +1,23 @@
+﻿using LivraisonCoteDor.network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivraisonCoteDor.readers
+{
+    public abstract class Reader
+    {
+        private string _filepath;
+
+
+        public Reader(string filepath)
+        {
+            this._filepath = filepath;
+        }
+
+        public abstract List<City> getExtractedCities();
+    }
+
+}
